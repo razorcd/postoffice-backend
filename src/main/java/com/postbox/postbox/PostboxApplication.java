2a11d1033032ac502d9b31d1cda3c76a1f345dbf
razor <razorcd@yahoo.com>
2017-08-25 17:28:15 +0200

    init

---
+++ src/main/java/com/postbox/postbox/PostboxApplication.java
@@ -0,0 +1,12 @@
+package com.postbox.postbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostboxApplication.class, args);
+	}
+}
