8b917ae393d07436c4ae6c8ef0de57abf147e729
Cristian Dugacicu <razorcd@yahoo.com>
2017-10-14 22:49:01 +0100

    adds cors wip

--- src/main/java/com/postbox/controler/BoxController.java
+++ src/main/java/com/postbox/controler/BoxController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -41,6 +42,7 @@ public class BoxController {
      *
       * @return list of all old requests
      */
+    @CrossOrigin(origins = "*", maxAge = 3600)
     @RequestMapping(value = "/requests", method = RequestMethod.GET)
     public @ResponseBody List<IncomingRequest> getRequests() {
         return incomingRequestServiceImpl.getAll();
