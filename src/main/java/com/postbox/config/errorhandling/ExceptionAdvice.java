59b5589bea658a15526f9f0b35d3ec333d45dfc9
Cristian Dugacicu <razorcd@yahoo.com>
2018-03-16 22:31:18 +0100

    adds ConstraintViolationException advice

--- src/main/java/com/postbox/config/errorhandling/ExceptionAdvice.java
+++ src/main/java/com/postbox/config/errorhandling/ExceptionAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,18 +17,34 @@ public class ExceptionAdvice {
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public CustomErrorDto elementNotFound(EntityNotFoundException e) {
+        e.printStackTrace();
         return new CustomErrorDto(HttpStatus.NOT_FOUND, e.getLocalizedMessage(), e);
     }
 
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public CustomErrorDto validationError(ValidationException e) {
+        e.printStackTrace();
         CustomErrorDto customErrorDto = new CustomErrorDto(HttpStatus.BAD_REQUEST, e.getLocalizedMessage(), e);
 
         List<CustomValidationErrorDTO> customValidationErrorDTOList = e.getExceptionFields().stream()
                 .map(exp -> new CustomValidationErrorDTO(exp.getObject(), exp.getField(), exp.getValue(), exp.getLocalizedMessage()))
                 .collect(Collectors.toList());
         customErrorDto.setValidationErrorDTOs(customValidationErrorDTOList);
+        return customErrorDto;
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public CustomErrorDto constraintValidationException(ConstraintViolationException e) {
+        e.printStackTrace();
+        CustomErrorDto customErrorDto = new CustomErrorDto(HttpStatus.BAD_REQUEST, e.getLocalizedMessage(), e);
+
+        List<CustomValidationErrorDTO> customValidationErrorDTOList = e.getConstraintViolations().stream()
+                .map(exp -> new CustomValidationErrorDTO(exp.getRootBeanClass(), exp.getPropertyPath().toString(), exp.getInvalidValue(), exp.getMessage()))
+                .collect(Collectors.toList());
+        customErrorDto.setValidationErrorDTOs(customValidationErrorDTOList);
+
         return customErrorDto;
     }
 }
